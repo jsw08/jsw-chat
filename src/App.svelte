6ecc5a2191fcb1a6ca7e28eb1a242be0b9683af5
Jurn Wubben <jurnwubben@gmail.com>
2022-06-25 15:16:25 +0200

    dark mode switcher

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,29 @@
 <script>
   import LoginPage from './lib/pages/Login.svelte'
   import { MaterialApp, Button, Icon } from 'svelte-materialify'
-  import { mdiBrightness6 } from '@mdi/js';
-  
+  import { mdiBrightness6, mdiYeast } from '@mdi/js';
+  import { onMount } from 'svelte'
+
   let login = true;
+  let darkmode = true;
+  let ls = {setItem: (y, yy) => {}};
+
+  $: setLS(darkmode)
+
+  let setLS = (theme) => {ls.setItem("theme", darkmode ? "darkon":"")}
+
+  onMount(() => {
+    ls = window.localStorage
+    darkmode = localStorage.getItem("theme") ? true:false
+  })
 </script>
 
-<Button style="position:absolute; right:5px; top:5px;"><Icon path={mdiBrightness6}/></Button>
-<MaterialApp  theme='light'>
+<MaterialApp  theme={darkmode ? "dark":"light"}>
+  <Button style="position:absolute; right:5px; top:5px;" on:click={() => {darkmode = !darkmode}}><Icon path={mdiBrightness6}/></Button>
   <div class="app" class:center={login}>
     {#if login}
       <LoginPage/>
-    {:else}
-
+    <!-- {:else} -->
     {/if}
   </div>
 </MaterialApp>
