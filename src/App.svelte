94a64dd1e1f7a4a0bb1143d06c5b008cc3bb18dc
Jurn Wubben <jurnwubben@gmail.com>
2022-06-25 16:23:10 +0200

    updatges

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script>
   import LoginPage from './lib/pages/Login.svelte'
+  import ChatPage from './lib/pages/Chat.svelte'
+
   import { MaterialApp, Button, Icon } from 'svelte-materialify'
   import { mdiBrightness6, mdiYeast } from '@mdi/js';
   import { onMount } from 'svelte'
 
   let login = true;
   let darkmode = true;
+  let usr = {}
   let ls = {setItem: (y, yy) => {}};
 
   $: setLS(darkmode)
-
   let setLS = (theme) => {ls.setItem("theme", darkmode ? "darkon":"")}
 
   onMount(() => {
@@ -22,8 +24,9 @@
   <Button style="position:absolute; right:5px; top:5px;" on:click={() => {darkmode = !darkmode}}><Icon path={mdiBrightness6}/></Button>
   <div class="app" class:center={login}>
     {#if login}
-      <LoginPage/>
-    <!-- {:else} -->
+      <LoginPage on:login={x => {x = x.detail; if (x.name) {usr = x; login = false;}}} darkmode={darkmode}/>
+    {:else}
+      <ChatPage/>
     {/if}
   </div>
 </MaterialApp>
--- src/lib/pages/Login.svelte
+++ src/lib/pages/Login.svelte
@@ -1,14 +1,19 @@
 <script>
+  export let darkmode;
   import  {Button, TextField}  from 'svelte-materialify';
+  import { createEventDispatcher } from 'svelte'
+  
+  let usr = {name:""}
+  const dispatch = createEventDispatcher();
 </script>
 
-<form>
+<form class:darkForm={darkmode} >
   <h6 class="font-weight-bold">JSW-CHAT</h6>
   <br>
-  <TextField solo placeholder="Username" />
+  <TextField solo placeholder="Username" bind:value={usr.name}/>
   <div>
     <Button class="red white-text" style="width:45%; display:inline;">cancel</Button>
-    <Button class="primary-color" style="width:45%; display:inline;">login</Button>
+    <Button class="primary-color" style="width:45%; display:inline;" on:click={() => {dispatch("login", usr)}}>login</Button>
   </div>
 </form>
 
@@ -22,5 +27,9 @@
     padding: 20px;
     text-align: center;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .darkForm {
+    border: 1px solid rgb(52, 52, 52);
   }
 </style>
