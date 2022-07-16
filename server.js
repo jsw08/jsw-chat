da0c1341f5d4b0cbf30cf6bd61b23707e94688be
Jurn Wubben <jurnwubben@gmail.com>
2022-07-16 19:40:52 +0200

    made the basis of the server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use("dist")
+
+io.on('connection', (socket) => {
+  socket.on("message",m => {
+    io.emit(m)
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
